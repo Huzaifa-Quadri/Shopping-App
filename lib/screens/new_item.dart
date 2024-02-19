ba3ec77895ebb81108ac532e37796a2ac7e97299
Huzaifa_q <huzaifaquadri1853@gmail.com>
2024-02-19 12:30:42 +0530

    Creating GLobal key to VALIDATE form feilds

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -9,6 +9,12 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formkey = GlobalKey<FormState>(); //Creating a Global key ;Form Specific
+
+  void _saveItem(){
+    _formkey.currentState!.validate();  // This will trigger validator function through key
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +24,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key : _formkey,
           child: Column(
             children: [
               TextFormField(
@@ -27,45 +34,11 @@ class _NewItemState extends State<NewItem> {
                     label: Text("Name"),
                   ),
                   validator: (value) {
-                    return 'Demo error msg...';
+                    if (value == null || value.isEmpty || value.trim().length<=1 || value.trim().length>50 ) {
+                      return "Enter between 1 to 50 characters";
+                    }
+                    return null;
                   }),
-//               Row(
-//                 crossAxisAlignment: CrossAxisAlignment.end,
-//                 children: [
-//                   Expanded(
-//                     child: TextFormField(
-//                       decoration: const InputDecoration(label: Text("Quantity")),
-//                       initialValue: "1",
-//                     ),
-//                   ),
-//                   const SizedBox(width: 8),
-//                   Expanded(
-//                     child: DropdownButtonFormField(
-//                       items: [
-//                       for (final category in categories.entries)
-//                         DropdownMenuItem(
-//                             child: Row(
-//                           children: [
-//                             Container(
-//                                   width: 16,
-//                                   height: 16,
-//                                   color: category.value.color,
-//                                 ),
-//                             const SizedBox(width: 6,),
-//                             Text(category.value.title)
-//                           ],
-//                         )),
-//                     ], onChanged: (value){}),
-//                   )
-//                 ],
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
@@ -75,6 +48,14 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                    if (value == null ||
+                    value.isEmpty || int.tryParse(value) == null || int.tryParse(value)!<=0) {  //int.tryParse(value) => returns null if string cannot be converted to number; we are checking entered value is a number or not
+                      return "Enter between 1 to 50 characters";
+                    }
+                    return null;
+                  }
                     ),
                   ),
                   const SizedBox(width: 8),
@@ -106,8 +87,14 @@ class _NewItemState extends State<NewItem> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextButton(onPressed: (){}, child: const Text("Reset")),
-                  ElevatedButton(onPressed: (){}, child: const Text("Add Item"))
+                  TextButton(onPressed:(){
+                     _formkey.currentState!.reset();
+                    }, 
+                    child: const Text("Reset")),
+                    
+                  ElevatedButton(
+                    onPressed: _saveItem, 
+                    child: const Text("Add Item"))
                 ],
               )
             ],
